1b6adda945657c23afe6e6c43f17c139f838926b
+ <+>
2023-09-03 17:29:38 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES
+    (1, 'Mr. Chocolate', 16, 'M', 'black', 'calm', 0),
+    (2, 'Rowdy', 16, 'M', 'black', 'energetic', 0),
+    (3, 'Tabitha', 16, 'F', 'black', 'friendly', 0),
+    (4, 'Sergeant Brown', 16, 'M', 'black', 'assertive', 0),
+    (5, 'Melissa', 16, 'F', 'black', 'gentle', 1),
+    (6, 'Grinch', 16, 'M', 'black', 'mischievous', 1),
+    (7, 'Wendy', 16, 'F', 'black', 'playful', 1),
+    (7, NULL, 16, 'F', 'black', 'playful', 1);
